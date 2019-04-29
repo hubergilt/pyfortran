eae3cab4494d6ae05f14742d2deb181ac29da857
Huber Paul Gilt Lopez <hubergilt@hotmail.com>
2019-04-28 21:07:05 -0500

    probando con gfortran en mac

---
+++ test_vectores.f90
@@ -0,0 +1,13 @@
+program main
+    use vectores
+
+    integer,parameter :: a = 100
+    double precision  :: u(a)
+    double precision  :: v(a)
+    double precision  :: resultdo
+
+    resultado = producto_escalar(n, u, v)
+
+    print *, "resultado", resultado
+
+end program main
